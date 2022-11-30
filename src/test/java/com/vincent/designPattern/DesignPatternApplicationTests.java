2d139ca6c809dedf19b9906f7d647da46470a783
Vincent <605842378@qq.com>
2022-11-30 21:02:50 +0800

    feat: 测试简单工厂方法

--- src/test/java/com/vincent/designPattern/DesignPatternApplicationTests.java
+++ src/test/java/com/vincent/designPattern/DesignPatternApplicationTests.java
@@ -1,5 +1,7 @@
 package com.vincent.designPattern;
 
+import com.vincent.designPattern.factory.SimpleMilkTeaFactory;
+import com.vincent.designPattern.store.MilkTeaStore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DesignPatternApplicationTests {
 
     @Test
-    void contextLoads() {
+    void simple_factory_test() {
+        MilkTeaStore milkTeaStore = new MilkTeaStore(new SimpleMilkTeaFactory());
+        milkTeaStore.createMilkTea("ChaBaiDao","淤泥波波");
     }
 
 }
