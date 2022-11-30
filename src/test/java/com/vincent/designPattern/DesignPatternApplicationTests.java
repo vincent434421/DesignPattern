a652c1cd3458f63f0830a6ea668f465bfe4c3baa
Vincent <605842378@qq.com>
2022-11-30 19:16:19 +0800

    first commit

---
+++ src/test/java/com/vincent/designPattern/DesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincent.designPattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
